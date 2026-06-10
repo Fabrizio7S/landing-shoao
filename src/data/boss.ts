4fb8fc2bf28f17324477c58f8de3ddc225822599
Fabrizio7S <leon.mallma14@gmail.com>
2026-06-10 17:27:47 -0500

    agregar videos de guias en boss

---
+++ src/data/boss.ts
@@ -0,0 +1,83 @@
+import type { boss } from "../types/Boss";
+
+export const bossesData: boss[] = [
+  {
+    name: "Lotus",
+    image: "/bosses/lotus.webp",
+  },
+  {
+    name: "Damian",
+    image: "/bosses/damian.webp",
+    videoId: "MioqDrwyRp8",
+  },
+  {
+    name: "Guardian Angel Slime",
+    image: "/bosses/guardian-angel-slime.webp",
+  },
+  {
+    name: "Lucid",
+    image: "/bosses/lucid.webp",
+  },
+  {
+    name: "Will",
+    image: "/bosses/will.webp",
+  },
+  {
+    name: "Gloom",
+    image: "/bosses/gloom.webp",
+  },
+  {
+    name: "Verus Hilla",
+    image: "/bosses/verus-hilla.webp",
+    videoId: "6JUW_qtpGvs",
+  },
+  {
+    name: "Malitia",
+    image: "/bosses/malitia.webp",
+  },
+  {
+    name: "Darknell",
+    image: "/bosses/darknell.webp",
+  },
+  {
+    name: "Black Mage",
+    image: "/bosses/black-mage.webp",
+  },
+  {
+    name: "Chosen Seren",
+    image: "/bosses/chosen-seren.webp",
+    videoId: "2idD656nFFI",
+  },
+  {
+    name: "Kalos",
+    image: "/bosses/kalos.webp",
+  },
+  {
+    name: "First Adversary",
+    image: "/bosses/first-adversary.webp",
+    videoId: "MioqDrwyRp8",
+  },
+  {
+    name: "Kaling",
+    image: "/bosses/kaling.webp",
+    videoId: "UoI19DIzeSE",
+  },
+  {
+    name: "Malefic Star",
+    image: "/bosses/malefic-star.webp",
+  },
+  {
+    name: "Limbo",
+    image: "/bosses/limbo.webp",
+    videoId: "gl0D-_Iyz4I",
+  },
+  {
+    name: "Baldrix",
+    image: "/bosses/baldrix.webp",
+    videoId: "SQD9CW5mQH8",
+  },
+  {
+    name: "Jupiter",
+    image: "/bosses/jupiter.webp",
+  },
+]
