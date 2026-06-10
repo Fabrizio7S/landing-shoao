989f0807eccbd37a535f5d2923c04a05cce6194d
Fabrizio7S <leon.mallma14@gmail.com>
2026-06-10 17:20:24 -0500

    feat: actualizar

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -1,52 +1,6 @@
 import type { Video } from "../types/Video";
 
 export const videosData: Video[] = [
-  //guias de bosses
-  {
-    videoId: "MioqDrwyRp8",
-    category: "Guía de Boss",
-  },
-  //verus hilla
-  {
-    videoId: "6JUW_qtpGvs",
-    category:"Guía de Boss",
-
-  },
-  //kaling fase 1
-  {
-    videoId :"UoI19DIzeSE",
-    category :"Guía de Boss",
-  },
-  //kaling fase 2
-  {
-    videoId:"ankVNbZBaLg",
-    category:"Guía de Boss",
-  },
-  //kaling fase 3
-  {
-    videoId :"BgURphDcqnM",
-    category :"Guía de Boss",
-  },
-  //seren
-  {
-    videoId:"2idD656nFFI",
-    category:"Guía de Boss",
-  },
-  //baldrix
-  {
-    videoId:"SQD9CW5mQH8",
-    category:"Guía de Boss",
-  },
-  //limbo
-  {
-    videoId:"gl0D-_Iyz4I",
-    category:"Guía de Boss",
-  },
-  //adversary
-  {
-    videoId:"MioqDrwyRp8",
-    category:"uía de Boss",
-  },
   {
     videoId: "fRtsMN3_cIs",
     category: "Guía / Tutorial",
