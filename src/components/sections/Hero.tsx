96caf629d77fd1ea98bf6f96e51047bf907cb84c
Fabrizio7S <leon.mallma14@gmail.com>
2026-06-14 15:50:43 -0500

    feat: Mejorar  hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,28 +1,39 @@
 import imagen from "../../assets/imagen.jpg";
+
 const Hero = () => {
   return (
-    <section className="bg-white min-h-screen flex items-center">
-      <div className="max-w-7xl mx-auto px-10 grid md:grid-cols-2 items-center gap-10">
+    <section className="bg-white py-20">
+      <div className="max-w-7xl mx-auto px-8 grid lg:grid-cols-2 gap-20 items-center">
 
         <div className="flex justify-center">
           <div className="relative">
-           
+
+            <div className="absolute inset-0 bg-yellow-300 rounded-full scale-110 -z-10"></div>
+
             <img
               src={imagen}
-              alt="imagen.jpg"
-              className="relative rounded-full w-80 h-80 object-cover"
+              alt="Jhohao"
+              className="w-80 h-80 lg:w-[450px] lg:h-[450px] object-cover rounded-full"
             />
           </div>
         </div>
 
         <div>
-          <h1 className="text-7xl font-bold text-gray-800 mb-6 py-10">
-            Welcome Mapleanos
+          <h1 className="text-6xl lg:text-8xl font-bold text-gray-900 leading-none mb-8">
+            Welcome
+            <br />
+            Mapleanos 
           </h1>
 
-          <a 
-          href="./Componentes/Videos"
-          className="bg-black text-white px-6 py-3 rounded-lg font-semibold hover:bg-gray-800 transition">
+          <p className="text-2xl text-gray-700 leading-relaxed max-w-xl mb-10">
+            Mi nombre es Jhohao jugador veterano 
+            que hace videos 🍁
+          </p>
+
+          <a
+            href="#bosses"
+            className="inline-block bg-black text-white px-8 py-4 rounded-full text-lg font-semibold hover:bg-gray-800 transition"
+          >
             Watch Videos
           </a>
         </div>
